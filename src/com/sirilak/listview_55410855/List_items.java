0bbbeb66c5da5924dd4f830891d61ffe1b7ea8d6
android55410855 <sirilakwailom@gmail.com>
2014-09-03 16:21:37 +0700

    waeo

---
+++ src/com/sirilak/listview_55410855/List_items.java
@@ -0,0 +1,21 @@
+package com.sirilak.listview_55410855;
+
+import android.graphics.drawable.Drawable;
+
+public class List_items {
+	private Drawable drawable;
+	private String title;
+	public Drawable getDrawable() {
+		return drawable;
+	}
+	public void setDrawable(Drawable drawable) {
+		this.drawable = drawable;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+}
